992c3637e79c3536836d450b16d69d7ee435db19
waouragh <waouragh@activeaero.com>
2019-09-04 13:27:36 -0400

    first commit

--- Auth.API/Controllers/ValuesController.cs
+++ Auth.API/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Auth.API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] {"value11", "value22"};
+            return new string[] {"value1", "value2"};
         }
 
         // GET api/values/5
